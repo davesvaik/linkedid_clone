c9b79ef9192a373b9f50f5e3ec3610a4e5c38211
davesvaik <d.svai93@gmail.com>
2022-01-23 20:49:46 +0100

    working on useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Header from './components/Header';
-import Home from './components/Home';
+import "./App.css";
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { getUserAuth } from "./actions";
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.getUserAuth();
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route exact path="/" element={<Login />}></Route>
 
-          <Route exact path="/" element={<Login />}>
-          </Route>
-
-          <Route exact path="/home" element={[<Header />, <Home />]}>
-          </Route>
-
+          <Route exact path="/home" element={[<Header />, <Home />]}></Route>
         </Routes>
       </Router>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserAuth: () => dispatch(getUserAuth),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
